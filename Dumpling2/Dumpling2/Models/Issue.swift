058121382a7b8ffac9eb98aa1848702142db6595
Lata Sadhwani <lata.sadhwani@gmail.com>
2015-03-03 21:27:55 +0530

    Making issues, articles and assets accessible via D2 methods

--- Dumpling2/Dumpling2/Models/Issue.swift
+++ Dumpling2/Dumpling2/Models/Issue.swift
@@ -30,19 +30,44 @@ public class Issue: RLMObject {
     
     //Get details for a specific key from custom meta of an issue
     public func getValue(key: NSString) -> AnyObject? {
-        let realm = RLMRealm.defaultRealm()
         
-        let predicate = NSPredicate(format: "globalId = '%@'", self.globalId)
-        var issues = Issue.objectsWithPredicate(predicate)
-        
-        if issues.count > 0 {
-            var issue: Issue =  issues.firstObject() as Issue
-            var metadata: AnyObject? = Helper.jsonFromString(issue.metadata)
-            if let metadataDict = metadata as? NSDictionary {
-                return metadataDict.valueForKey(key)
-            }
+        var metadata: AnyObject? = Helper.jsonFromString(self.metadata)
+        if let metadataDict = metadata as? NSDictionary {
+            return metadataDict.valueForKey(key)
         }
         
+        return nil
+    }
+    
+    
+    // MARK: Public methods
+    
+    //Delete an issue
+    public class func deleteIssue(appleId: NSString) {
+        let realm = RLMRealm.defaultRealm()
+        
+        let predicate = NSPredicate(format: "appleId = '%@'", appleId)
+        var issues = Issue.objectsWithPredicate(predicate)
+        
+        //Delete all assets and articles for the issue
+        if issues.count == 1 {
+            //older issue
+            var currentIssue = issues.firstObject() as Issue
+            //Delete all articles and assets if the issue already exists
+            Article.deleteArticlesFor(currentIssue.globalId)
+            
+            //Delete issue
+            realm.beginWriteTransaction()
+            realm.deleteObjects(currentIssue)
+            realm.commitWriteTransaction()
+        }
+    }
+    
+    public class func getNewestIssue() -> Issue? {
+        let realm = RLMRealm.defaultRealm()
+        
+        //TODO: Check why arraySortedByProperty doesnt work
+        //var results = Issue.allObjects().arraySortedByProperty("publishedDate", ascending: true)
         return nil
     }
 }
